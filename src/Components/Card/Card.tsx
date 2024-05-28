219a753a75b5b83a8309b157cb594626bb57a01c
Pavel Pomozov <pomozov@raveinfosys.com>
2024-05-28 10:43:37 +0300

    added primereact lib

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import styles from "./Card.module.css";
+import { Button } from "primereact/button";
 
 // разбить на мелкую дату
 const Card = ({ data }) => {
   const style = { borderColor: "#b215bd" };
-  return <div className={styles.cardWrapper} style={style}></div>;
+  return (
+    <div className={styles.cardWrapper} style={style}>
+      <Button label="Submit" />
+    </div>
+  );
 };
 
 export default Card;
